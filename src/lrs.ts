8e3c73a8b704e9be3a5890f33c56bba84c5c7d7f
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2018-01-30 17:10:57 -0800

    Split network layer code into separate file.

--- src/lrs.ts
+++ src/lrs.ts
@@ -1,17 +1,7 @@
-import * as urlRe from "./urlRe";
-import { request, IRequestOptions } from "@esri/arcgis-rest-request";
-import {
-  SqlDateFormat,
-  IField,
-  ILayerInfo,
-  MeasureUnits,
-  esriLocatingStatusG2M,
-  dateToTimeInstant,
-  esriLocatingStatusM2G
-} from "./common";
-import { INetworkLayer } from "./layers";
-import UrlFormatError from "./UrlFormatError";
-import { SpatialReference, Point, Geometry } from "arcgis-rest-api";
+import {IRequestOptions, request} from '../node_modules/@esri/arcgis-rest-request/src/request';
+import { SqlDateFormat, IField, ILayerInfo } from './common';
+import UrlFormatError from './UrlFormatError';
+import { lrsServer, allLayers } from './urlRe';
 
 export interface ILrsInfo {
   id: string;
@@ -78,10 +68,6 @@ export interface ILrsServiceInfo {
   redlineInfos?: IRedlineInfo[];
 }
 
-export interface INetworkLayersResponse {
-  networkLayers: INetworkLayer[];
-}
-
 /**
  * This is to be used for requests that only consist of a URL
  * and does not have any other parameters (besides f=json,
@@ -113,12 +99,12 @@ export function validateUrl(url: string, re: RegExp) {
  * @param re RegExp used for validating the URL.
  * @see {@link https://esri.github.io/arcgis-rest-js/api/request/request/|ArcGIS REST JS: request}
  */
-async function makeValidatedRequest(
+export async function makeValidatedRequest(
   requestOptions: IUrlRequestOptions,
   re: RegExp
 ) {
   let { url } = requestOptions;
-  url = validateUrl(url, urlRe.lrsServer);
+  url = validateUrl(url, lrsServer);
   return request(url, requestOptions);
 }
 
@@ -130,7 +116,7 @@ async function makeValidatedRequest(
 export async function getLrsServiceInfo(
   requestOptions: IUrlRequestOptions
 ): Promise<ILrsServiceInfo> {
-  return makeValidatedRequest(requestOptions, urlRe.lrsServer);
+  return makeValidatedRequest(requestOptions, lrsServer);
 }
 
 /**
@@ -141,137 +127,5 @@ export async function getLrsServiceInfo(
 export async function getAllLayers(
   requestOptions: IUrlRequestOptions
 ): Promise<IAllLayersResponse> {
-  return makeValidatedRequest(requestOptions, urlRe.allLayers);
-}
-
-/**
- * Fetches information about the network layers.
- * @param requestOptions
- * @see {@link http://roadsandhighwayssample.esri.com/roads/api/networklayer.html|Network Layer}
- */
-export async function getNetworkLayers(
-  requestOptions: IUrlRequestOptions
-): Promise<INetworkLayersResponse> {
-  return makeValidatedRequest(requestOptions, urlRe.networkLayers);
-}
-
-export interface IG2MInputLocation {
-  routeId?: string | null;
-  geometry: Point;
-}
-
-export interface IG2MOutputLocation {
-  status: esriLocatingStatusG2M;
-  results: Array<{
-    routeId: string;
-    measure: number;
-    geometryType: "esriGeometryPoint";
-    geometry: Point;
-  }>;
-}
-
-export interface IG2MOutput {
-  unitsOfMeasure: MeasureUnits;
-  spatialReference: SpatialReference;
-  locations: IG2MOutputLocation[];
-}
-
-export interface IG2MOptions extends IUrlRequestOptions {
-  locations: Array<IG2MInputLocation | number[]>;
-  tolerance?: number;
-  temporalViewDate?: Date;
-  inSR?: number;
-  outSR?: number;
-  gdbVersion?: string;
-}
-
-export interface IG2MLocation {
-  routeId: string;
-}
-
-export interface IG2MPointLocation extends IG2MLocation {
-  routeId: string;
-  measure: number;
-}
-
-export interface IG2MLineLocation extends IG2MLocation {
-  routeId: string;
-  toRouteId?: string;
-  fromMeasure: number;
-  toMeasure: number;
-}
-
-export interface IM2GOptions extends IUrlRequestOptions {
-  locations: IG2MLocation[];
-  temporalViewDate: Date;
-  outSR?: number;
-  gdbVersion?: string;
-}
-
-/**
- * Converts an array of two numbers to an input location object for
- * use with the REST API. If the input is already in the expected format,
- * it will be returned as-is.
- * @param location
- * @example
- * const locations = numbers.map(coordsToLocation);
- * @throws TypeError if the input is an array with less than two elements.
- */
-function coordsToLocation(location: IG2MInputLocation | number[]) {
-  if (Array.isArray(location)) {
-    if (location.length < 2) {
-      throw TypeError("Item must be an array with two or more elements.");
-    }
-    return {
-      geometry: {
-        x: location[0],
-        y: location[1]
-      }
-    };
-  }
-  return location;
-}
-
-export interface IM2GOutput {
-  spatialReference: SpatialReference;
-  locations: Array<{
-    status: esriLocatingStatusM2G;
-    routeId: string;
-    geometryType: "esriGeometryPoint" | "esriGeometryPolyline";
-    geometry: Geometry;
-  }>;
-}
-
-/**
- * Finds route measures closest to the input points.
- * @param options
- * @see {@link http://roadsandhighwayssample.esri.com/roads/api/geometrytomeasure.html|Geometry to Measure (Operation)}
- */
-export async function geometryToMeasure(
-  options: IG2MOptions
-): Promise<IG2MOutput> {
-  // make sure double[] are converted to location objects.
-  options.locations = options.locations.map(coordsToLocation);
-  if (options.temporalViewDate) {
-    options.temporalViewDate = dateToTimeInstant(
-      options.temporalViewDate
-    ) as any;
-  }
-  return makeValidatedRequest(options, urlRe.geometryToMeasure);
-}
-
-/**
- * Returns the geometries for the input route measures.
- * @param options
- * @see {@link http://roadsandhighwayssample.esri.com/roads/api/measuretogeometry.html|Measure to Geometry (Operation)}
- */
-export async function measureToGeometry(
-  options: IM2GOptions
-): Promise<IM2GOutput> {
-  if (options.temporalViewDate) {
-    options.temporalViewDate = dateToTimeInstant(
-      options.temporalViewDate
-    ) as any;
-  }
-  return makeValidatedRequest(options, urlRe.measureToGeometry);
+  return makeValidatedRequest(requestOptions, allLayers);
 }
